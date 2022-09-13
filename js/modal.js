e729b6b85abf95d901bf71ee28432647b901433a
dobryimelnik <marjan.om84@gmail.com>
2022-09-13 17:41:46 +0300

    added modal

---
+++ js/modal.js
@@ -0,0 +1,14 @@
+const modalBtns = document.querySelectorAll('.more');// кнопка learn more
+const modal = document.querySelector('.modal');// модальное окно с изначально поставленным классом hidden
+
+for(let modalBtn of modalBtns) {
+    modalBtn.addEventListener('click', () => {
+        modal.classList.remove('hidden');
+    })
+    modal.addEventListener('click', (event) => {
+        const target = event.target;// получает элемент в модальном окне на который кликнули мышью
+        if(target.classList.contains('overlay') || target.classList.contains('modal__close')) { // при клике на элемент с классом overlay или modal__close...
+            modal.classList.add('hidden');// модальное окно скрывается
+        }
+    })
+}
